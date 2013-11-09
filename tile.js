9cec0fcf116ac0d59bf4343179ee90ffc7d06d3f
Vincent Heuken <vincent@vincentheuken.com>
2013-11-08 16:15:33 -0800

    added generateHTML function

--- tile.js
+++ tile.js
@@ -1,15 +1,33 @@
-var TILE_UNCLICKED_HTML = '<div class="tile_unclicked"></div>';
-var TILE_CLICKED_HTML   = '<div class="tile_clicked"></div>';
+var TILE_UNCLICKED_CLASS = 'tile_unclicked';
+var TILE_CLICKED_CLASS   = 'tile_clicked';
 
 function Tile (x, y, isMine)
 {
+    this.generateHTML = function () 
+    {
+        var html = '<div ';
+        
+        if (this.isClicked === true)
+        {
+            html += 'class="' + TILE_UNCLICKED_CLASS + '"';
+        }
+        else
+        {
+            html += 'class="' + TILE_CLICKED_CLASS + '"';
+        }        
+        
+        html += '></div>';
+        
+        return html;
+    }
+    
     this.x = x;
     this.y = y;
     this.isMine = isMine;
     this.isClicked = true;
     this.isFlagged = false;
     
-    this.html = TILE_UNCLICKED_HTML;
+    this.html = this.generateHTML();
 }
 
 Tile.prototype.click = function ()
